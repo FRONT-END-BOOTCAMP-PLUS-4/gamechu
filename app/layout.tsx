a637d3b0d8660a576a9d3a3fb60d7c22d867e27f
imtaejunk <imtaejunk@github.com>
2025-05-15 17:20:42 +0900

    [feat/#23] gamecard, gamelist 컴포넌트, layout 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,23 @@
-// app/layout.tsx
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./globals.css";
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="ko">
-      <body className="font-sans bg-background-400 text-font-100">
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="ko">
+            <body className="font-sans bg-background-400 text-font-100">
+                <Header />
+                <main className="w-full max-w-[1480px] mx-auto px-10">
+                    <div className="w-full max-w-[1400px] mx-auto">
+                        {children}
+                    </div>
+                </main>
+                <Footer />
+            </body>
+        </html>
+    );
 }
