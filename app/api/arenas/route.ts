d184c954e329615d5d5151f7801534e0a8e817f0
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-05-26 17:50:12 +0900

    [feat/#79] 투기장 Usecase 작성 완료, API 작성중

--- app/api/arenas/route.ts
+++ app/api/arenas/route.ts
@@ -1,16 +1,57 @@
+import { ArenaListDto } from "@/backend/arena/application/usecase/dto/ArenaListDto";
+import { GetArenaDto } from "@/backend/arena/application/usecase/dto/GetArenaDto";
+import { GetArenaUsecase } from "@/backend/arena/application/usecase/GetArenaUsecase";
 import { ArenaRepository } from "@/backend/arena/domain/repositories/ArenaRepository";
 import { PrismaArenaRepository } from "@/backend/arena/infra/repositories/prisma/PrismaArenaRepository";
+import { MemberRepository } from "@/backend/member/domain/repositories/MemberRepository";
+import { PrismaMemberRepository } from "@/backend/member/infra/repositories/prisma/PrismaMemberRepository";
+import { VoteRepository } from "@/backend/vote/domain/repositories/VoteRepository";
+import { PrismaVoteRepository } from "@/backend/vote/infra/repositories/prisma/PrismaVoteRepository";
 import { getAuthUserId } from "@/utils/GetAuthUserId.server";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
     try {
+        const memberId = await getAuthUserId();
+
         // get query parameters from URL
         const url = new URL(request.url);
-        const status = url.searchParams.get("status") || null;
+        const currentPage: number = Number(
+            url.searchParams.get("currentPage") || 1
+        );
+        const status: number = Number(url.searchParams.get("status"));
+        const mine: boolean = Boolean(url.searchParams.get("mine"));
+        const pageSize: number = Number(request.headers.get("pageSize")!);
+
+        if (!memberId && mine) {
+            return NextResponse.json(
+                { error: "투기장 조회 권한이 없습니다." },
+                { status: 401 }
+            );
+        }
 
         // set up repositories and usecases
         const arenaRepository: ArenaRepository = new PrismaArenaRepository();
+        const memberRepository: MemberRepository = new PrismaMemberRepository();
+        const voteRepository: VoteRepository = new PrismaVoteRepository();
+
+        const getArenaUsecase = new GetArenaUsecase(
+            arenaRepository,
+            memberRepository,
+            voteRepository
+        );
+
+        // set up query DTO
+        const getArenaDto: GetArenaDto = new GetArenaDto(
+            { currentPage, status, mine },
+            memberId,
+            pageSize
+        );
+
+        const arenaListDto: ArenaListDto = await getArenaUsecase.execute(
+            getArenaDto
+        );
+        return NextResponse.json(arenaListDto);
     } catch (error) {
         console.error(`Error fetching arenas: ${error}`);
         return NextResponse.json(
