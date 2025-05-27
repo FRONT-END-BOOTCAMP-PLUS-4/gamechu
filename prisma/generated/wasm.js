b529401808b5d6eb43c537ddb6f76656b21ca808
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-05-27 10:16:55 +0900

    [feat/#79] DB 변경 - 마지막 출석 date로 저장하게 변경

--- prisma/generated/wasm.js
+++ prisma/generated/wasm.js
@@ -129,9 +129,9 @@ exports.Prisma.MemberScalarFieldEnum = {
   birthDate: 'birthDate',
   isMale: 'isMale',
   score: 'score',
+  lastAttendedDate: 'lastAttendedDate',
   createdAt: 'createdAt',
-  deletedAt: 'deletedAt',
-  last_attended_date: 'last_attended_date'
+  deletedAt: 'deletedAt'
 };
 
 exports.Prisma.GameScalarFieldEnum = {
