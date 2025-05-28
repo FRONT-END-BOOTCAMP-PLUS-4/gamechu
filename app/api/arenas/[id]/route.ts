90868cbc053c045a0275134d7d785a7992cf0a5a
wojin57 <57216021+wojin57@users.noreply.github.com>
2025-05-28 17:07:04 +0900

    [feat/#89] 아레나 POST, DELETE API 구현, prisma/generated 버전관리 제외

--- app/api/arenas/[id]/route.ts
+++ app/api/arenas/[id]/route.ts
@@ -1,7 +1,15 @@
+import { DeleteArenaUsecase } from "@/backend/arena/application/usecase/DeleteArenaUsecase";
 import { GetArenaDetailUsecase } from "@/backend/arena/application/usecase/GetArenaDetailUsecase";
 import { UpdateArenaStatusUsecase } from "@/backend/arena/application/usecase/UpdateArenaStatusUsecase";
+import { ArenaRepository } from "@/backend/arena/domain/repositories/ArenaRepository";
 import { PrismaArenaRepository } from "@/backend/arena/infra/repositories/prisma/PrismaArenaRepository";
 import { NextRequest, NextResponse } from "next/server";
+
+type RequestParams = {
+    params: {
+        id: number;
+    };
+};
 
 export async function GET(
     req: NextRequest,
@@ -18,9 +26,9 @@ export async function GET(
     try {
         const result = await usecase.execute(arenaId);
         return NextResponse.json(result);
-    } catch (err) {
+    } catch (error) {
         return NextResponse.json(
-            { error: "Failed to fetch participants" },
+            { error: `Failed to fetch participants: ${error}` },
             { status: 500 }
         );
     }
@@ -39,9 +47,42 @@ export async function PATCH(
     try {
         await usecase.execute(arenaId, status, challengerId); // challengerId 없으면 undefined
         return NextResponse.json({ success: true });
-    } catch (err: any) {
+    } catch (err: unknown) {
+        let errorMessage = "에러 발생";
+        if (err instanceof Error) {
+            errorMessage = err.message;
+        }
         return NextResponse.json(
-            { success: false, error: err.message || "에러 발생" },
+            { success: false, error: errorMessage },
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(request: Request, { params }: RequestParams) {
+    try {
+        const { id } = await params;
+
+        const arenaRepository: ArenaRepository = new PrismaArenaRepository();
+        const deleteArenaUsecase: DeleteArenaUsecase = new DeleteArenaUsecase(
+            arenaRepository
+        );
+
+        await deleteArenaUsecase.execute(Number(id));
+
+        return NextResponse.json(
+            { message: "투기장 삭제 성공" },
+            { status: 200 }
+        );
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                { message: error.message || "투기장 삭제 실패" },
+                { status: 400 }
+            );
+        }
+        return NextResponse.json(
+            { message: "알 수 없는 오류 발생" },
             { status: 500 }
         );
     }
