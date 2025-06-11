644a17fa5178c5cc28f3619340fc18dff5370327
Kwon Woojin <wojin57@gmail.com>
2025-06-11 16:29:41 +0900

    [refactor] notification 백엔드 리팩토링

--- backend/notification-record/domain/repositories/NotificationRecordRepository.ts
+++ backend/notification-record/domain/repositories/NotificationRecordRepository.ts
@@ -1,11 +1,16 @@
 import { NotificationRecord } from "@/prisma/generated";
 import { NotificationRecordFilter } from "./filters/NotificationRecordFilter";
+import { UpdateNotificationRecordDto } from "../../application/usecase/dto/UpdateNotificationRecordDto";
+
+export type CreateNotificationRecordInput = Omit<NotificationRecord, "id">;
 
 export interface NotificationRecordRepository {
     count(filter: NotificationRecordFilter): Promise<number>;
     findAll(filter: NotificationRecordFilter): Promise<NotificationRecord[]>;
     findById(id: number): Promise<NotificationRecord | null>;
-    save(record: NotificationRecord): Promise<NotificationRecord>;
-    update(record: NotificationRecord): Promise<NotificationRecord>;
+    save(record: CreateNotificationRecordInput): Promise<NotificationRecord>;
+    update(
+        updateNotificationRecordDto: UpdateNotificationRecordDto
+    ): Promise<NotificationRecord>;
     deleteById(id: number): Promise<void>;
 }
--- backend/notification-type/domain/repositories/NotificationTypeRepository.ts
+++ backend/notification-type/domain/repositories/NotificationTypeRepository.ts
@@ -1,10 +1,12 @@
 import { NotificationType } from "@/prisma/generated";
+import { UpdateNotificationTypeDto } from "../../application/usecase/dto/UpdateNotificationTypeDto";
 
+export type CreateNotificationTypeInput = Omit<NotificationType, "id">;
 export interface NotificationTypeRepository {
     count(): Promise<number>;
     findAll(): Promise<NotificationType[]>;
     findById(id: number): Promise<NotificationType | null>;
-    save(type: NotificationType): Promise<NotificationType>;
-    update(type: NotificationType): Promise<NotificationType>;
+    save(type: CreateNotificationTypeInput): Promise<NotificationType>;
+    update(updateType: UpdateNotificationTypeDto): Promise<NotificationType>;
     deleteById(id: number): Promise<void>;
 }
