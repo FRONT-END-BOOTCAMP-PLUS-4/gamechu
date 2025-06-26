82bcc12d3e811b05ab90bc23aeb4e917f46e215e
roast <roastt1@naver.com>
2025-06-26 15:45:14 +0900

    [refactor/#183] interface Dto로 분리

--- backend/chatting/application/usecase/GetChattingUsecase.ts
+++ backend/chatting/application/usecase/GetChattingUsecase.ts
@@ -1,20 +1,17 @@
-import { Chatting } from "@/prisma/generated";
 import { ChattingRepository } from "../../domain/repositories/ChattingRepository";
 import { ChattingFilter } from "../../domain/repositories/filters/ChattingFilter";
 import { GetChattingDto } from "./dto/GetChattingDto";
+import { GetChattingResultDto } from "./dto/GetChattingResultDto";
 
 // 상수는 유스케이스 내부에서도 정의 가능
 const MAX_SEND_COUNT = 5;
 
-interface GetChattingResult {
-    chats: Chatting[];
-    remainingSends: number;
-}
-
 export class GetChattingUsecase {
     constructor(private chattingRepo: ChattingRepository) {}
 
-    async execute(getChattingDto: GetChattingDto): Promise<GetChattingResult> {
+    async execute(
+        getChattingDto: GetChattingDto
+    ): Promise<GetChattingResultDto> {
         const { arenaId, memberId } = getChattingDto;
         const filterChats = new ChattingFilter(arenaId, null); // 채팅을 filter할 때는 memberId가 null
         const filterCount = new ChattingFilter(arenaId, memberId);
--- backend/chatting/application/usecase/dto/ChattingDto.ts
+++ backend/chatting/application/usecase/dto/ChattingDto.ts
@@ -1,9 +1,9 @@
 export class ChattingDto {
     constructor(
+        public id: number,
         public memberId: string,
         public arenaId: number,
         public content: string,
-        public createdAt: Date = new Date(), // 기본값 설정
-        public id?: number
+        public createdAt: Date = new Date() // 기본값 설정
     ) {}
 }
