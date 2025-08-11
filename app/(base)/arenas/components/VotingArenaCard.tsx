ac208c0878aadb6a3c3ca6f0915cb1af6a85fb12
Kwon Woojin <wojin57@gmail.com>
2025-08-11 18:58:26 +0900

    [style/#196] 대기중, 투표 진행중, 토론 진행중 투기장에 반응형 디자인 적용

--- app/(base)/arenas/components/VotingArenaCard.tsx
+++ app/(base)/arenas/components/VotingArenaCard.tsx
@@ -7,10 +7,12 @@ import { useRouter } from "next/navigation";
 type VotingArenaCardProps = {
     id: number;
     title: string;
+
     creatorNickname: string;
     creatorScore: number;
     challengerNickname: string | null;
     challengerScore: number | null;
+
     voteEndDate: Date;
     voteCount: number;
 };
@@ -23,35 +25,40 @@ export default function VotingArenaCard(props: VotingArenaCardProps) {
 
     return (
         <div
-            className="w-[440px] transform gap-4 rounded-2xl border border-transparent bg-background-300 p-4 text-white shadow-md transition-all duration-200 hover:scale-[1.01] hover:cursor-pointer hover:border-purple-500 hover:shadow-lg hover:shadow-purple-500/30"
+            className="flex h-full w-full transform flex-col gap-4 rounded-2xl border border-transparent bg-background-300 p-4 text-white shadow-md transition-all duration-200 hover:scale-[1.01] hover:cursor-pointer hover:border-purple-500 hover:shadow-lg hover:shadow-purple-500/30"
             onClick={onClickHandler}
         >
-            <div className="flex items-center justify-between">
-                <div className="line-clamp-2 text-lg font-bold">
+            <div className="flex flex-row items-start justify-between gap-1">
+                <div className="line-clamp-2 min-h-[3.5rem] break-keep text-lg font-bold">
                     {props.title}
                 </div>
-                <div className="rounded-full bg-purple-500 px-3 py-1 text-xs font-semibold text-white">
+                <div className="flex h-6 flex-shrink-0 items-center rounded-full bg-purple-500 px-3 text-xs font-semibold">
                     투표중
                 </div>
             </div>
 
-            <div className="m-2 mt-2 flex items-center justify-between gap-4 text-sm text-gray-100">
-                <div className="flex items-center gap-2">
-                    <span>{props.creatorNickname}</span>
+            <div className="flex items-center justify-between text-sm text-gray-100">
+                {/* 작성자 */}
+                <div className="flex min-w-0 flex-shrink items-center gap-2 overflow-hidden">
+                    <span className="max-w-[100px] truncate whitespace-nowrap">
+                        {props.creatorNickname}
+                    </span>
                     <TierBadge score={props.creatorScore} size="sm" />
                 </div>
 
                 <span className="mx-2 text-gray-400">vs</span>
 
                 {/* 도전자 */}
-                <div className="flex items-center gap-2">
-                    <span>{props.challengerNickname}</span>
+                <div className="flex min-w-0 flex-shrink items-center gap-2 overflow-hidden">
+                    <span className="max-w-[100px] truncate whitespace-nowrap">
+                        {props.challengerNickname}
+                    </span>
                     <TierBadge score={props.challengerScore || 0} size="sm" />
                 </div>
             </div>
 
-            <div className="mt-4 flex items-center justify-between text-sm text-gray-300">
-                <div className="flex items-center gap-1">
+            <div className="mt-2 flex items-center justify-between">
+                <div className="flex items-center gap-1 text-sm text-gray-400">
                     <Image
                         src="/icons/infoCalendar.svg"
                         alt="달력 아이콘"
@@ -60,9 +67,7 @@ export default function VotingArenaCard(props: VotingArenaCardProps) {
                         className="object-contain"
                     />
                     <span className="text-gray-400">
-                        투표 종료:{" "}
                         {props.voteEndDate.toLocaleDateString("ko-KR", {
-                            year: "numeric",
                             month: "long",
                             day: "numeric",
                             hour: "2-digit",
@@ -78,9 +83,7 @@ export default function VotingArenaCard(props: VotingArenaCardProps) {
                         height={18}
                         className="object-contain"
                     />
-                    <span className="text-gray-400">
-                        {props.voteCount}명 투표
-                    </span>
+                    <span className="text-gray-400">{props.voteCount}명</span>
                 </div>
             </div>
         </div>
