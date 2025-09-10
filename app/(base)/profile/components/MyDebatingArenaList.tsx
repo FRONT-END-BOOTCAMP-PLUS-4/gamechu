d9acef90c88aa52c7953508a1e4646b71c9cb386
이규원 <woni011420@gmail.com>
2025-09-10 16:35:10 +0900

    [style/#199] 스크롤 삭제 및 UI 변경 및 마이 아레나리스트 브레이크 포인트 변경

--- app/(base)/profile/components/MyDebatingArenaList.tsx
+++ app/(base)/profile/components/MyDebatingArenaList.tsx
@@ -51,9 +51,8 @@ export default function MyDebatingArenaList() {
 
     return (
         <div className="flex w-full flex-col items-center gap-6">
-            {/* ✅ 반응형 + 스크롤 */}
-            <div className="w-full overflow-x-auto overflow-y-hidden">
-                <div className="grid grid-cols-1 gap-6 break-keep px-1 min-[821px]:min-w-[640px] min-[821px]:grid-cols-2">
+            <div className="w-full">
+                <div className="grid grid-cols-1 gap-6 px-1 md:grid-cols-2">
                     {arenaListDto.arenas.map((arena) => (
                         <DebatingArenaCard
                             key={arena.id}
@@ -64,7 +63,7 @@ export default function MyDebatingArenaList() {
                             challengerNickname={arena.challengerNickname}
                             challengerScore={arena.challengerScore}
                             debateEndDate={new Date(arena.debateEndDate)}
-                            showBadgeIconOnly={true}
+                            showBadgeIconOnly={false}
                         />
                     ))}
                 </div>
