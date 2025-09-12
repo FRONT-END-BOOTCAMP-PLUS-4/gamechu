aeb47490378a85fd425a29ed7a626868f02c47c5
roast <roastt1@naver.com>
2025-09-12 14:13:16 +0900

    [style/#212] 게임 헤더 및 정보 컴포넌트 lg 이상/미만 별도 레이아웃 적용

--- app/(base)/games/[gameId]/containers/GameDetailContainer.tsx
+++ app/(base)/games/[gameId]/containers/GameDetailContainer.tsx
@@ -1,10 +1,11 @@
-import GameTitleCardBase from "../components/GameTitleCardBase";
+import GameHeaderImageSection from "../components/GameHedaerImageSection";
 import GameInfoCard from "../components/GameInfoCard";
 import ClientContentWrapper from "../components/ClientContentWrapper";
 import { GetGameDetailUsecase } from "@/backend/game/application/usecase/GetGameDetailUsecase";
 import { GamePrismaRepository } from "@/backend/game/infra/repositories/prisma/GamePrismaRepository";
 import { getAuthUserId } from "@/utils/GetAuthUserId.server";
 import { PrismaReviewRepository } from "@/backend/review/infra/repositories/prisma/PrismaReviewRepository";
+import GameHeaderInfoSection from "../components/GameHeaderInfoSection";
 
 interface Props {
     gameId: number;
@@ -22,22 +23,59 @@ export default async function GameDetailContainer({ gameId }: Props) {
     const game = await gameDetailUsecase.execute(gameId);
 
     return (
-        <div className="min-h-screen space-y-10 bg-background-400 pb-10 text-font-100">
-            <div className="mx-auto flex w-full max-w-[1400px] flex-col justify-between gap-6 px-4 pt-10 lg:flex-row lg:px-0 lg:pt-20">
-                <div className="flex w-full flex-col gap-6 lg:flex-row">
-                    <div className="flex-[3]">
-                        <GameTitleCardBase
-                            image={game.thumbnail ?? ""}
+        <div className="min-h-screen space-y-10 bg-background-400 pb-10 pt-6 text-font-100">
+            {/* lg미만 레이아웃*/}
+            <div className="mx-auto flex w-full max-w-[1400px] flex-col gap-4 rounded-3xl bg-background-100 p-8 lg:hidden">
+                <GameHeaderImageSection
+                    image={game.thumbnail ?? ""}
+                    title={game.title}
+                />
+                <div className="flex">
+                    <div className="flex-1">
+                        <GameHeaderInfoSection
                             title={game.title}
-                            developer={game.developer ?? ""}
-                            releaseDate={game.releaseDate ?? ""}
-                            gameId={game.id}
-                            rating={game.rating ?? undefined}
+                            developer={game.developer}
+                            rating={game.rating}
+                            releaseDate={game.releaseDate}
+                            gameId={gameId}
                             viewerId={viewerId ?? ""}
                         />
                     </div>
+                    <div className="flex-1">
+                        <GameInfoCard
+                            platforms={game.platforms}
+                            genres={game.genres}
+                            themes={game.themes}
+                            wishCount={game.wishCount}
+                            reviewCount={game.reviewCount}
+                        />
+                    </div>
+                </div>
+            </div>
 
-                    <div className="flex-[1]">
+            {/* lg이상 레이아웃 */}
+            <div className="mx-auto hidden max-w-[1400px] flex-col gap-6 px-0 lg:flex">
+                <div className="flex gap-6">
+                    {/* flex-[3_2] = flex-grow:2, flex-shrink:2 (flex-shrink는 공간 부족 시 줄어드는 비율) */}
+                    <div className="flex min-w-0 flex-[3_2] gap-6 rounded-3xl bg-background-100 p-4">
+                        <div className="flex min-w-0 flex-1 items-center justify-center p-4">
+                            <GameHeaderImageSection
+                                image={game.thumbnail ?? ""}
+                                title={game.title}
+                            />
+                        </div>
+                        <div className="min-w-0 flex-1">
+                            <GameHeaderInfoSection
+                                title={game.title}
+                                developer={game.developer}
+                                rating={game.rating}
+                                releaseDate={game.releaseDate}
+                                gameId={gameId}
+                                viewerId={viewerId ?? ""}
+                            />
+                        </div>
+                    </div>
+                    <div className="min-w-0 flex-[1_1] rounded-3xl bg-background-100 p-4">
                         <GameInfoCard
                             platforms={game.platforms}
                             genres={game.genres}
