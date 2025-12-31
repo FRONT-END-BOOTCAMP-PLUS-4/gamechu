c20c588b493ee991ddb92bb8e51d95025a3025b4
roast <roastt1@naver.com>
2025-12-31 23:41:34 +0900

    [fix/#241] 투기장 제목 잘림/내용 줄바꿈 표시 오류 및 레이아웃 수정

--- app/(base)/arenas/[id]/components/ArenaDetailHeader.tsx
+++ app/(base)/arenas/[id]/components/ArenaDetailHeader.tsx
@@ -5,38 +5,42 @@ import Image from "next/image";
 
 export default function ArenaDetailHeader() {
     const arenaDetail = useArenaStore((state) => state.arenaData);
+
     return (
         <div className="flex w-full max-w-[1000px] flex-col gap-4">
             {/* 상단: 제목 & 게시자 */}
-            <div className="flex flex-wrap items-center justify-between gap-2">
+            <div className="flex flex-wrap items-start justify-between gap-2">
                 {/* 제목 */}
-                <div className="flex min-w-0 animate-fade-in-left items-center">
+                <div className="flex animate-fade-in-left items-center gap-2">
                     <Image
-                        src="/icons/arena2.svg"
+                        src="/icons/arena.svg"
                         alt="투기장 아이콘"
-                        width={40}
-                        height={40}
+                        width={28}
+                        height={28}
+                        className={"flex-shrink-0 sm:h-10 sm:w-10"}
                     />
-                    <h2 className="ml-2 max-w-[400px] truncate text-h2 font-bold text-font-100">
+                    <h2 className="break-words text-lg font-bold text-font-100 sm:text-2xl">
                         {arenaDetail?.title ?? "투기장 제목"}
                     </h2>
                 </div>
+
                 {/* 게시자 */}
-                <div className="flex min-w-0 animate-fade-in-right items-center gap-2">
+                <div className="flex min-w-0 flex-shrink-0 animate-fade-in-right items-center gap-2">
                     <Image
                         src="/icons/teamA.svg"
                         alt="게시자 아이콘"
-                        width={40}
-                        height={40}
+                        width={28}
+                        height={28}
+                        className={"sm:h-10 sm:w-10"}
                     />
-                    <h2 className="max-w-[150px] truncate text-h2 font-bold text-font-100">
+                    <h2 className="max-w-[150px] truncate text-lg font-bold text-font-100 sm:text-2xl">
                         {arenaDetail?.creatorName ?? "게시자"}
                     </h2>
                 </div>
             </div>
 
             {/* 하단: 내용 */}
-            <div className="max-h-64 animate-fade-in-up overflow-y-auto rounded-xl bg-background-300 p-4 text-body text-font-100">
+            <div className="max-h-64 animate-fade-in-up overflow-y-auto whitespace-pre-line rounded-xl bg-background-300 p-4 text-sm text-font-100 sm:text-lg">
                 {arenaDetail?.description ?? "투기장 내용"}
             </div>
         </div>
