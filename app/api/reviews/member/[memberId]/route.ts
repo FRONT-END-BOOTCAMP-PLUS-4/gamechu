59e6429f38af6ebd19bad8dfa611b2f40495fb92
이규원 <woni011420@gmail.com>
2026-01-27 15:23:50 +0900

    [feat/#242] 타 사용자의 리뷰 갯수 및 목록 출력 API 연동

--- app/api/reviews/member/[memberId]/route.ts
+++ app/api/reviews/member/[memberId]/route.ts
@@ -1,16 +1,18 @@
-// 로그인 유저의 리뷰 조회
-
+//비 로그인 유저의 리뷰 조회(app\api\reviews\member\[memberId]\route.ts)
 import { NextResponse } from "next/server";
 import { GetReviewsByMemberIdUsecase } from "@/backend/review/application/usecase/GetReviewsByMemberIdUsecase";
 import { PrismaReviewRepository } from "@/backend/review/infra/repositories/prisma/PrismaReviewRepository";
-import { getAuthUserId } from "@/utils/GetAuthUserId.server";
 
 const usecase = new GetReviewsByMemberIdUsecase(new PrismaReviewRepository());
 
-export async function GET() {
-    const memberId = await getAuthUserId();
+export async function GET(
+    request: Request,
+    { params }: { params: Promise<{ memberId: string }> }
+) {
+    const { memberId } = await params;
+
     if (!memberId) {
-        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        return NextResponse.json({ message: "Not Found" }, { status: 404 });
     }
 
     const result = await usecase.execute(memberId);
