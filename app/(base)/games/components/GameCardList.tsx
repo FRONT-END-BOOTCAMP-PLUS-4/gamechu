320472d8b31e9db0fc8ea1a51bd66ab8b949015d
roast <roastt1@naver.com>
2026-02-28 04:21:56 +0900

    [fix/#247] 게임 검색 결과 없는 경우 안내 문구 UI 렌더

--- app/(base)/games/components/GameCardList.tsx
+++ app/(base)/games/components/GameCardList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GameCard from "./GameCard";
+import Image from "next/image";
 
 interface GameCardData {
     id: number;
@@ -16,6 +17,27 @@ interface GameCardListProps {
 }
 
 export default function GameCardList({ games }: GameCardListProps) {
+    if (games.length === 0) {
+        return (
+            <div className="flex w-full flex-col items-center justify-center py-28 text-center">
+                <Image
+                    src="/images/empty-game.svg"
+                    width={120}
+                    height={120}
+                    alt="No data image"
+                />
+
+                <h3 className="mb-2 text-xl font-bold text-gray-300">
+                    검색 결과가 없습니다
+                </h3>
+                <p className="text-sm text-gray-400">
+                    선택한 필터 조건에 맞는 게임을 찾을 수 없어요.
+                    <br />
+                    다른 필터를 시도해 보세요.
+                </p>
+            </div>
+        );
+    }
     return (
         <div className="mb-8 grid grid-cols-2 gap-4 sm:gap-6 lg:grid-cols-5">
             {games.map((game) => (
